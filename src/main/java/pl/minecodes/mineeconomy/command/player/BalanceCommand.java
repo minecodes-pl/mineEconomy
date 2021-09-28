09f1cea16f76649bc663b710b2bbe188d1d23601
artur <biuro@minecodes.pl>
2021-09-28 22:11:06 +0200

    :tada: Created player balance command.

---
+++ src/main/java/pl/minecodes/mineeconomy/command/player/BalanceCommand.java
@@ -0,0 +1,32 @@
+package pl.minecodes.mineeconomy.command.player;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Default;
+import com.google.common.collect.ImmutableMap;
+import eu.okaeri.injector.annotation.Inject;
+import org.bukkit.entity.Player;
+import pl.minecodes.mineeconomy.data.configuration.Configuration;
+import pl.minecodes.mineeconomy.data.configuration.Messages;
+import pl.minecodes.mineeconomy.profile.Profile;
+import pl.minecodes.mineeconomy.profile.ProfileService;
+import pl.minecodes.mineeconomy.util.MessageUtil;
+import pl.minecodes.mineeconomy.util.PlaceholderUtil;
+
+@CommandAlias("balance|money|bal|account")
+public class BalanceCommand extends BaseCommand {
+
+    @Inject private Messages messages;
+    @Inject private Configuration configuration;
+    @Inject private ProfileService profileService;
+
+    @Default
+    public void onPlayerBalanceCheck(Player player) {
+        Profile profile = this.profileService.getProfile(player.getUniqueId());
+        MessageUtil.sendMessage(player, PlaceholderUtil.replace(this.messages.getBalanceCheck(),
+                ImmutableMap.of(
+                        "balance", String.valueOf(profile.getBalance()),
+                        "currency", this.configuration.getCurrency())));
+    }
+
+}
