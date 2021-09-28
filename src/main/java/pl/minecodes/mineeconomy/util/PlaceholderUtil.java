a08ea7da513ef841c2f9b4c37503e7c7a0428bd5
artur <biuro@minecodes.pl>
2021-09-28 22:10:32 +0200

    :tada: Created util for replace placeholders in strings.

---
+++ src/main/java/pl/minecodes/mineeconomy/util/PlaceholderUtil.java
@@ -0,0 +1,21 @@
+package pl.minecodes.mineeconomy.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class PlaceholderUtil {
+
+    public static String replace(String string, Map<String, Object> placeholders) {
+        AtomicReference<String> atomicString = new AtomicReference<>(string);
+        placeholders.forEach((key, value) -> atomicString.set(atomicString.get().replace("{" + key + "}", String.valueOf(value))));
+        return atomicString.get();
+    }
+
+    public static List<String> replace(List<String> list, Map<String, Object> placeholders) {
+        List<String> replacedList = new ArrayList<>();
+        list.forEach(string -> replacedList.add(replace(string, placeholders)));
+        return replacedList;
+    }
+}
