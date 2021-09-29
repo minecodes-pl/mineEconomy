622c33ab61c497733bac1199e28f451608dbeb5e
artur <biuro@minecodes.pl>
2021-09-29 23:31:43 +0200

    :data: Created Vault hook.

---
+++ src/main/java/pl/minecodes/mineeconomy/hook/VaultHook.java
@@ -0,0 +1,29 @@
+package pl.minecodes.mineeconomy.hook;
+
+import eu.okaeri.injector.annotation.Inject;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class VaultHook {
+
+    @Inject
+    private Logger logger;
+    @Inject
+    private JavaPlugin plugin;
+    @Inject
+    private VaultManager provider;
+
+    public void registerHook() {
+        Bukkit.getServicesManager().register(Economy.class, this.provider, plugin, ServicePriority.Highest);
+        this.logger.info("Successfully register Vault hook!");
+    }
+
+    public void unregisterHook() {
+        Bukkit.getServicesManager().unregister(Economy.class, this.provider);
+        this.logger.info("Successfully unregister Vault hook!");
+    }
+}
