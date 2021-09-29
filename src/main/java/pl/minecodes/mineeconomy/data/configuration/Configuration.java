3705d193d3af3d96c3e306724002e95d2c535894
artur <biuro@minecodes.pl>
2021-09-29 08:35:00 +0200

    :tada: Remove lombok from configuration and add pluralize library for currency.

--- src/main/java/pl/minecodes/mineeconomy/data/configuration/Configuration.java
+++ src/main/java/pl/minecodes/mineeconomy/data/configuration/Configuration.java
@@ -2,14 +2,37 @@ package pl.minecodes.mineeconomy.data.configuration;
 
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.annotation.Comment;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import eu.okaeri.pluralize.Pluralize;
 
-@Data
-@EqualsAndHashCode(callSuper = false)
+import java.util.Locale;
+
 public class Configuration extends OkaeriConfig {
 
-    @Comment("Nazwa waluty")
-    private String currency = "Iskry";
+    @Comment("Nazwy waluty w odmianach.")
+    private String currency = "Iskre|Iskry|Iskier";
+    @Comment("Początkowy stan konta gracza.")
+    private double startBalance = 10;
 
+
+    public String getCurrency(double value) {
+        String[] currencySplitter = this.currency.split("\\|");
+        if (currencySplitter.length > 0 && currencySplitter.length < 3) {
+            return currencySplitter[0];
+        }
+        return Pluralize.pluralize(Locale.forLanguageTag("pl"), (int) value, currencySplitter[0], currencySplitter[1], currencySplitter[2]);
+    }
+
+    @Deprecated
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public double getStartBalance() {
+        return startBalance;
+    }
+
+    @Deprecated
+    public void setStartBalance(double startBalance) {
+        this.startBalance = startBalance;
+    }
 }
