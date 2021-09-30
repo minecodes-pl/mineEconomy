9a26827f91b67d2e5c49993a1250486c6dc06cbe
arturekdev <koeckiartur@gmail.com>
2021-09-30 08:49:28 +0200

    :tada: Created hook to PlaceholderAPI.

---
+++ src/main/java/pl/minecodes/mineeconomy/hook/placeholderapi/PlaceholderAPIHook.java
@@ -0,0 +1,51 @@
+package pl.minecodes.mineeconomy.hook.placeholderapi;
+
+import eu.okaeri.injector.annotation.Inject;
+import me.clip.placeholderapi.PlaceholderHook;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import pl.minecodes.mineeconomy.data.configuration.Configuration;
+import pl.minecodes.mineeconomy.profile.Profile;
+import pl.minecodes.mineeconomy.profile.ProfileService;
+
+public class PlaceholderAPIHook extends PlaceholderExpansion {
+
+    @Inject
+    private JavaPlugin plugin;
+    @Inject
+    private Configuration configuration;
+    @Inject
+    private ProfileService profileService;
+
+    @Override
+    public String getIdentifier() {
+        return this.plugin.getDescription().getName();
+    }
+
+    @Override
+    public String getAuthor() {
+        return this.plugin.getDescription().getAuthors().toString();
+    }
+
+    @Override
+    public String getVersion() {
+        return this.plugin.getDescription().getVersion();
+    }
+
+
+    @Override
+    public String onRequest(OfflinePlayer player, String parameter) {
+        Profile profile = this.profileService.getProfile(player.getUniqueId());
+        if (parameter.equals("balance")) {
+            switch (this.configuration.getCurrencyPositionVault()) {
+                case AHEAD:
+                    return this.configuration.getCurrency(profile.getBalance()) + profile.getBalance();
+                case BEHIND:
+                    return profile.getBalance() + this.configuration.getCurrency(profile.getBalance());
+            }
+        }
+        return "";
+    }
+}
